e8a77f1715707ef36e54d6903e5178e9e9361f8c
sylcuh <121727107+sylcuh@users.noreply.github.com>
2024-07-21 21:12:27 +0530

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+Fluent:Notify({
+  Title = "Bypassers, inc.",
+  Content = "Loaded! Thanks for using Bypasser!",
+  SubContent = "sylcuh - https://byfronsucks.vercel.app", -- Optional
+  Duration = 5 -- Set to nil to make the notification not disappear
+})
