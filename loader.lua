786e84a9c781e12602634c54d4707bc722f198de
sylcuh <121727107+sylcuh@users.noreply.github.com>
2024-07-21 21:13:16 +0530

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,17 @@
+-- funcs
+local ver = "1.0.0"
+function iy() return loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))() end
+function LoadIY() return iy() end
+function infyield() return iy() end
+function identifyexecutor() return 'ByfronBypasser', 'v1.0.0' end
+function getexecutorname() return identifyexecutor() end
+function executorname() return identifyexecutor() end
+function executorversion() return select(identifyexecutor(), 2) end
+function getexecutorversion() return executorversion() end
+function printidentity() print("Current identity is 8") return end
+function getthreadidentity() return '8' end
+function getthreadcontext() return getthreadidentity() end
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 Fluent:Notify({
   Title = "Bypassers, inc.",
